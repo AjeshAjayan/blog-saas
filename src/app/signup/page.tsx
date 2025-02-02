bf31c074ad96c5e0b0b24d56f3425b1bf1806f6b
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2025-02-02 16:03:00 +0530

    Login and Sign up - UI

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,18 +4,23 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-	title: "Welcome back..!",
-	description: "Login to your account",
+	title: "Get Started",
+	description: "Create an account to get started",
 	robots: "noindex, nofollow",
 };
 
-
-export default function Login() {
+export default function SignUp() {
 	return (
 		<div className="xs:h-full xs:flex xs:items-end xs:p-8 xs:bg-[url('/illustration.jpg')] xs:bg-cover xs:bg-[position:-45px_-180px]">
 			<main className="xs:z-10 xs:w-full ">
 				<form className="xs:w-full xs:h-3/4 xs:bg-white xs:p-6 xs:flex xs:flex-col xs:gap-3 rounded">
-					<h1>Login to your Account</h1>
+					<h1>Sign Up</h1>
+					<BInput type="text"
+						id="fullname"
+						name="fullname"
+						placeholder="Full Name"
+						label="Full Name"
+					/>
 					<BInput type="email"
 						id="email"
 						name="email"
@@ -28,8 +33,14 @@ export default function Login() {
 						placeholder="Password"
 						label="Password"
 					/>
+					<BInput type="password"
+						id="confirm-password"
+						name="confirmPassword"
+						placeholder="Confirm Password"
+						label="Confirm Password"
+					/>
 					<BButton type="submit">Login</BButton>
-					<Link href="/signup">Create an account</Link>
+					<p>Already have an account? <Link href="/login">Login</Link></p>
 				</form>
 			</main>
 		</div>
