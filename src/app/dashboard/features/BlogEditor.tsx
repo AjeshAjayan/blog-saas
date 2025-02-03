69cc3771c1388272505431abf06b688bd46028ed
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2025-02-03 17:17:57 +0530

    Feature - able to preview blogs while editing

--- src/app/dashboard/features/BlogEditor.tsx
+++ src/app/dashboard/features/BlogEditor.tsx
@@ -11,6 +11,7 @@ import rehypeSanitize from "rehype-sanitize";
 import { BInput } from "@/components/BInput"
 import { BSuccessText } from "@/components/BSuccessText"
 import { toast } from "react-toastify"
+import { Preview } from "./Preview"
 
 const MDEditor = dynamic(() => import("@uiw/react-md-editor").then((mod) => mod.default), { ssr: false })
 
@@ -21,7 +22,7 @@ export default function BlogEditor() {
     const [showAutoSaveText, setShowAutoSaveText] = useState(false);
     const [title, setTitle] = useState('');
     const [blogId, setBlogId] = useState(0);
-
+    const [showPreview, setShowPreview] = useState(false);
 
     const [saveBlogPost] = useMutation(SAVE_BLOG, { client })
     const [publishBlogPost] = useMutation(PUBLISH_BLOG, { client })
@@ -72,46 +73,63 @@ export default function BlogEditor() {
         setTitle(e.target.value)
     }
 
+    const handlePreview = () => void setShowPreview(p => !p);
+
     return (
         <div className="flex flex-col gap-4">
-            <div className=" flex gap-4 items-center">
-                <BButton
-                    onClick={handlePublish}
+            <div className=" flex gap-4 items-center w-full justify-between">
+                <div className="flex gap-4 items-center">
+                    <BButton
+                        onClick={handlePublish}
+                        type="button"
+                        className="hidden max-w-40 md:block"
+                        disabled={blogId === 0}
+                    >
+                        Publish
+                    </BButton>
+                    <BSuccessText className={`${showAutoSaveText ? ' opacity-100' : 'opacity-0'}`}>Auto save...</BSuccessText>
+                </div>
+                <BButton 
+                    onClick={handlePreview}
                     type="button"
                     className="hidden max-w-40 md:block"
-                    disabled={blogId === 0}
-                >
-                    Publish
+                    disabled={!Boolean(content)}>
+                        { showPreview ? 'Edit' : 'Preview'}
                 </BButton>
-                <BSuccessText className={`${showAutoSaveText ? ' opacity-100' : 'opacity-0'}`}>Auto save...</BSuccessText>
             </div>
-            <BInput
-                type="text"
-                id="blog-title"
-                label="Let's see what you got...!"
-                placeholder="Provide an attractive title"
-                value={title}
-                onChange={handleTitleOnChange}
-            />
-            <div className="hidden md:flex flex-col">
-                <div className="w-full">
-                    <MDEditor
-                        value={content}
-                        onChange={handleOnChange}
-                        height={800}
-                        previewOptions={{
-                            rehypePlugins: [[rehypeSanitize]],
-                        }}
-                        textareaProps={{
-                            placeholder: 'Please enter Markdown text',
-                        }}
+            {
+                showPreview
+                ? <Preview>{ content }</Preview>
+                : <>
+                    <BInput
+                        type="text"
+                        id="blog-title"
+                        label="Let's see what you got...!"
+                        placeholder="Provide an attractive title"
+                        value={title}
+                        onChange={handleTitleOnChange}
                     />
-                </div>
-            </div>
-            <div className="md:hidden">
-                <p>You need bigger screen to create post</p>
-                <p>Screen width must be greater than or equal to 768px</p>
-            </div>
+                    <div className="hidden md:flex flex-col">
+                        <div className="w-full">
+                            <MDEditor
+                                value={content}
+                                onChange={handleOnChange}
+                                height={800}
+                                previewOptions={{
+                                    rehypePlugins: [[rehypeSanitize]],
+                                }}
+                                textareaProps={{
+                                    placeholder: 'Please enter Markdown text',
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="md:hidden">
+                        <p>You need bigger screen to create post</p>
+                        <p>Screen width must be greater than or equal to 768px</p>
+                    </div>
+                </>
+            }
         </div>
     )
 }
--- src/app/dashboard/features/Preview.tsx
+++ src/app/dashboard/features/Preview.tsx
@@ -1,13 +1,13 @@
 import ReactMarkdown from "react-markdown"
 
 type PreviewProps = {
-    content: string
+    children: string
 }
 
-export const Preview = ({ content }: PreviewProps) => {
+export const Preview = ({ children }: PreviewProps) => {
     return (
         <div className="w-full border p-4 overflow-auto h-full">
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown>{children}</ReactMarkdown>
         </div>
     )
 }
